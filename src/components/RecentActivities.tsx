8bb1a8d7418d8b83eace45860639ffaae287232f
Carsten Bröckert <cabroe@gmail.com>
2025-07-16 16:44:21 +0200

    Implementierung des UI-Zustands in RecentActivities, TicktacActions und SettingsHeader. Hinzufügen von Toggle-Funktionen für Menüs und Anpassung der ARIA-Attribute zur Verbesserung der Zugänglichkeit. Verwendung von <Link> für die Navigation in SettingsHeader.

--- src/components/RecentActivities.tsx
+++ src/components/RecentActivities.tsx
@@ -1,11 +1,36 @@
 import React from "react";
+import { useUiStore } from "../store/useUiStore";
 
-const RecentActivities: React.FC = () => (
-  <div className="nav-item d-flex me-1 recent-activities">
-    <a className="btn btn-icon px-0" title="Letzte Tätigkeit neu starten">
-      <i className="fas fa-repeat"></i>
-    </a>
-  </div>
-);
+const RecentActivities: React.FC = () => {
+  const { open, close, isOpen } = useUiStore();
+
+  function handleRecentActivitiesToggle() {
+    if (isOpen("recentActivitiesMenu")) {
+      close();
+    } else {
+      open("recentActivitiesMenu");
+    }
+  }
+
+  return (
+    <div className="nav-item d-flex me-1 recent-activities">
+      <a
+        className="btn btn-icon px-0"
+        title="Letzte Tätigkeit neu starten"
+        onClick={handleRecentActivitiesToggle}
+        aria-expanded={isOpen("recentActivitiesMenu") ? "true" : "false"}
+      >
+        <i className="fas fa-repeat"></i>
+      </a>
+      {/* Beispiel: Menüanzeige */}
+      {/* {isOpen("recentActivitiesMenu") && (
+        <div className="dropdown-menu show">
+          <span className="dropdown-item" onClick={handleRecentActivityClick}>Letzte Tätigkeit 1</span>
+          <span className="dropdown-item" onClick={handleRecentActivityClick}>Letzte Tätigkeit 2</span>
+        </div>
+      )} */}
+    </div>
+  );
+};
 
 export default RecentActivities; 

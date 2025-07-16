6288abb84751cd338b5271d4a337b2a40fa88f77
Carsten Bröckert <cabroe@gmail.com>
2025-07-16 19:06:36 +0200

    Umbenennung der App-Komponente in AppRoutes und Entfernung des BrowserRouter. Die Routen werden nun direkt innerhalb von Routes definiert, um die Struktur der Anwendung zu vereinfachen und die Lesbarkeit zu verbessern.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {
   Route,
   Routes,
-  BrowserRouter as Router,
 } from "react-router-dom";
 import Times from "./pages/Times";
 import Settings from "./pages/Settings";
@@ -12,19 +11,17 @@ import MainTemplate from "./templates/MainTemplate";
 import NotFound from "./pages/NotFound";
 import "./App.css";
 
-function App() {
+function AppRoutes() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<MainTemplate><Dashboard /></MainTemplate>} />
-        <Route path="/times" element={<MainTemplate><Times /></MainTemplate>} />
-        <Route path="/settings" element={<MainTemplate><Settings /></MainTemplate>} />
-        <Route path="/modal-demo" element={<MainTemplate><ModalDemo /></MainTemplate>} />
-        <Route path="/toast-demo" element={<MainTemplate><ToastDemo /></MainTemplate>} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainTemplate><Dashboard /></MainTemplate>} />
+      <Route path="/times" element={<MainTemplate><Times /></MainTemplate>} />
+      <Route path="/settings" element={<MainTemplate><Settings /></MainTemplate>} />
+      <Route path="/modal-demo" element={<MainTemplate><ModalDemo /></MainTemplate>} />
+      <Route path="/toast-demo" element={<MainTemplate><ToastDemo /></MainTemplate>} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   );
 }
 
-export default App;
+export default AppRoutes;

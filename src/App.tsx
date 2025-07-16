d0c8a84cbf7f9dae091b6d67c9df52f9def3edb9
Carsten Bröckert <cabroe@gmail.com>
2025-07-16 17:45:35 +0200

    Umstrukturierung der App-Komponente zur Verwendung von MainTemplate für die Seiteninhalte. Entfernen der Sidebar- und HeaderWithRoute-Komponenten sowie der PageHeaderContent-Funktion, um die Codebasis zu bereinigen und die Struktur zu vereinfachen.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import {
   Route,
   Routes,
-  useLocation,
   BrowserRouter as Router,
 } from "react-router-dom";
 import Times from "./pages/Times";
@@ -9,48 +8,19 @@ import Settings from "./pages/Settings";
 import Dashboard from "./pages/Dashboard";
 import ModalDemo from "./pages/ModalDemo";
 import ToastDemo from "./pages/ToastDemo";
-import Sidebar from "./components/Sidebar";
-import PageHeader from "./components/PageHeader";
-import HeaderWithRoute from "./components/HeaderWithRoute";
-import { getHeaderComponentForRoute } from "./utils/getHeaderComponentForRoute";
+import MainTemplate from "./templates/MainTemplate";
 import "./App.css";
-
-// PageHeaderContent ist für die dynamische Auswahl und Anzeige des passenden PageHeaders zuständig.
-// Sie liest anhand der aktuellen Route (pathname) aus der zentralen routeConfig (pageTitles) die passende Header-Komponente aus
-// und rendert diese im zentralen PageHeader. So bleibt die Header-Logik für alle Seiten flexibel und zentral wartbar.
-function PageHeaderContent() {
-  const location = useLocation();
-  const { pathname } = location;
-  const HeaderComponent = getHeaderComponentForRoute(pathname);
-  if (HeaderComponent) {
-    return <HeaderComponent />;
-  }
-  return null;
-}
 
 function App() {
   return (
     <Router>
-      <Sidebar />
-      <HeaderWithRoute />
-      <div className="page-wrapper">
-        <div className="container-fluid">
-          <PageHeader>
-            <PageHeaderContent />
-          </PageHeader>
-        </div>
-        <div className="page-body">
-          <div className="container-fluid">
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/times" element={<Times />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/modal-demo" element={<ModalDemo />} />
-              <Route path="/toast-demo" element={<ToastDemo />} />
-            </Routes>
-          </div>
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<MainTemplate><Dashboard /></MainTemplate>} />
+        <Route path="/times" element={<MainTemplate><Times /></MainTemplate>} />
+        <Route path="/settings" element={<MainTemplate><Settings /></MainTemplate>} />
+        <Route path="/modal-demo" element={<MainTemplate><ModalDemo /></MainTemplate>} />
+        <Route path="/toast-demo" element={<MainTemplate><ToastDemo /></MainTemplate>} />
+      </Routes>
     </Router>
   );
 }
